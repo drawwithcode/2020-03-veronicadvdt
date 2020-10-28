63d2bd6438e08e644809ddc6036f728d44c9175a
veronicadvdt <veronicadetoni@gmail.com>
2020-10-28 14:38:10 +0100

    Sound_Imm_V001

--- sketch.js
+++ sketch.js
@@ -1,10 +1,25 @@
 function preload(){
-  // put preload code here
+  myImage1 = loadImage("./assets/Brokensignal.png");
+  myImage2 = loadImage("./assets/Trippybackground.png");
+  myImage3 = loadImage("./assets/Woman.png");
+  myImage4 = loadImage("./assets/Universe.png");
+  myImage5 = loadImage("./assets/Flower.png");
+  myImage6 = loadImage("./assets/Hand.png");
+  myImage7 = loadImage("./assets/TV.png");
+
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+  background("lavender");
+  image(myImage1, width/3 , height/6);
+  image(myImage2, width/3 , height/6);
+  image(myImage3, width/3 , height/6);
+  image(myImage4, width/3 , height/6);
+  image(myImage5, width/3 , height/6);
+  image(myImage6, width/3 , height/6);
+  image(myImage7, width/3 , height/6);
+
 }
 
 function draw() {
